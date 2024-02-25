11b6f5427391ca988670a8d2dac2dff717f8fac3
Vishal Mahajan <vism06@gmail.com>
2024-02-25 14:45:28 +0530

    Register Page Added

---
+++ EventHive/State/RegisterState.py
@@ -0,0 +1,48 @@
+from rxconfig import config
+from typing import List
+import reflex as rx
+import re
+
+
+
+class RegisterFormState(rx.State):
+    # These track the user input real time for validation
+    user_entered_email: str
+    user_entered_password: str
+
+    # These are the submitted data
+    usertype: str
+    email: str
+    password: str
+    fname: str
+    lname: str
+    form_data: dict = {}
+    
+    @rx.var
+    def invalid_email(self) -> bool:
+        return not re.match(
+            r"[^@]+@(student\.sfit\.ac\.in|sfit\.ac\.in)$", self.user_entered_email
+        )
+
+    @rx.var
+    def invalid_password(self) -> bool:
+        return len(self.user_entered_password) < 8
+    
+
+
+    @rx.var
+    def input_invalid(self) -> bool:
+        return (
+            self.invalid_email
+            or self.invalid_password
+        )
+
+    def handle_submit(self, form_data: dict):
+        """Handle the form submit."""
+        self.usertype = form_data.get("usertype")
+        self.fname = form_data.get("fname")
+        self.lname = form_data.get("lname")
+        self.email = form_data.get("email")
+        self.password = form_data.get("password")
+        print(self.usertype, self.email, self.password)
+        print(form_data)
--- EventHive/pages/register.py
+++ EventHive/pages/register.py
@@ -1,15 +1,129 @@
-
 from rxconfig import config
-from typing import List
 import reflex as rx
+from ..State.RegisterState import RegisterFormState
 
 
 
-
-Who: List[str] = ["Student","Commitee"]
 
 def register() -> rx.Component:
     return rx.center(
-      
+        rx.flex(
+            rx.heading("Register", align="center"),
+            rx.text("Fill the Following Details",),
+            rx.form.root(
+            rx.flex(
+                rx.select.root(
+                    rx.select.trigger(placeholder="Register As"),
+                    rx.select.content(
+                        rx.select.group(
+                            rx.select.item("Student", value="student"),
+                            rx.select.item("Coordinator", value="Coordinator"),
+                            name="usertype",
+                        ),
+                    ),
+                    name="usertype",
+                ),
+                rx.grid(
+                    rx.form.field(
+                        rx.form.control(
+                            rx.input.input(
+                                placeholder="First Name",
+                                name="fname",
+                                size="2",
+                            ),
+                            as_child=True,
+                        ),
+                        name="fname",
+                    ),
+                    rx.form.field(
+                        rx.form.control(
+                            rx.input.input(
+                                placeholder="Last Name",
+                                name="lname",
+                                size="2",
+                            ),
+                            as_child=True,
+                        ),
+                        name="lname",
+                    ),
+                    columns="2",
+                    spacing="1",
+                    width="100%",
+                ),
+                rx.form.field(
+                        rx.form.control(
+                            rx.input.input(
+                                placeholder="Email Address",
+                                on_change=RegisterFormState.set_user_entered_email,
+                                name="email",
+                                size="2",
+                            ),
+                            as_child=True,
+                        ),
+                        rx.form.message(
+                            "Enter email address with domain @student.sfit.ac.in",
+                            match="valueMissing",
+                            force_match=RegisterFormState.invalid_email,
+                            color="var(--red-11)",
+                            font_size="0.85em",
+                        ),
+                    name="email",
+                    server_invalid=RegisterFormState.invalid_email,
+                ),
+                rx.form.field(
+                    rx.form.control(
+                        rx.input.input(
+                            placeholder="Password",
+                            on_change=RegisterFormState.set_user_entered_password,
+                            name="password",
+                            type="password",
+                            size="2",
+                            min_length=8,
+                        ),
+                        as_child=True,
+                    ),
+                    rx.form.message(
+                        "Password should be at least 8 characters long",
+                        match="tooShort",
+                        force_match=RegisterFormState.invalid_password,
+                        color="var(--red-11)",
+                        font_size="0.85em",
+                    ),
+                    name="password",
+                ),
+                rx.form.submit(
+                    rx.button(
+                        "Submit",
+                        disabled=RegisterFormState.input_invalid,
+                        color_scheme="teal",
+                    ),
+                    as_child=True,
+                ),
+                direction="column",
+                spacing="4",
+                width="20em",
+            ),
+            on_submit=RegisterFormState.handle_submit,
+            reset_on_submit=True,
+        ),
+        rx.divider(),
+        rx.text(
+            "Already Registered? ",
+             rx.link(
+                rx.button("Login", color_scheme="teal"),
+                href="/",
+                color_scheme="teal",
+                button=True,
+                ),
+             padding="0.5em 0 0 0",
+            ),
+            padding="2em",
+            class_name="bg-white backdrop-blur-md rounded-lg shadow-lg",
+            direction="column",
+            spacing="2",
+            align="center",
+        ),
+        width="100%",
+        height="100vh",
+        bg_color="#0a192f",
     )
-
